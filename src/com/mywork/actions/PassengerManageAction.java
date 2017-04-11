1d0678c2c34eff02eb6623b3a9a98f91021bce7b
chazen996 <chazen@qq.com>
2017-04-11 13:13:16 +0800

    修复了前端提交搜索后后台抓不到数据的问题

--- src/com/mywork/actions/PassengerManageAction.java
+++ src/com/mywork/actions/PassengerManageAction.java
@@ -16,74 +16,7 @@ import com.mywork.dao.SubDao;
 
 public class PassengerManageAction {
 	private Map<String, Object> map = new LinkedHashMap<String, Object>();
-
-	public Map<String, Object> getMap() {
-		return map;
-	}
-	//产生PassengerManage.jsp页面数据
-	public String GenerateData() throws IOException, SQLException{
-		ArrayList<Passenger> alist = new ArrayList<Passenger>();
-		int count = 0;
-		SubDao passengerdao,passengerdao2;
-		passengerdao = new SubDao();
-		passengerdao2 = new SubDao();
-		passengerdao.openDB();
-		passengerdao2.openDB();
-		String sql = "select * from t_passenger";
-		ResultSet rs = passengerdao.executeQuery(sql);
-		while(rs.next()){
-			Passenger p=new Passenger();
-			//得到当前乘客的购票数量和购票金额
-			String sql2 = "select count(*) as count,sum(price_purc) as sum from t_order where pass_id="+rs.getInt("pass_id");
-			ResultSet rs2 = passengerdao2.executeQuery(sql2);
-			if(rs2.next()){
-				p.setPass_count(rs2.getInt("count"));
-				p.setPass_amount(rs2.getDouble("sum"));
-			}
-			
-			p.setPass_id(rs.getInt("pass_id"));
-			p.setPass_name(rs.getString("pass_name"));
-			p.setPass_age(rs.getInt("pass_age"));
-			if(rs.getInt("pass_sex")==1){
-				p.setPass_sex("男");
-			}else{
-				p.setPass_sex("女");
-			}
-			p.setPass_idcard(rs.getString("pass_idcard"));
-			p.setPass_passport(rs.getString("pass_passport"));
-			p.setPass_phone(rs.getString("pass_phone"));
-			p.setPass_email(rs.getString("pass_email"));
-			alist.add(p);
-			count++;
-		}
-		passengerdao.closeDB();
-		passengerdao2.closeDB();
-		map.put("status", "success");
-		map.put("totals",count);
-		map.put("data", alist);
-		ServletActionContext.getResponse().setHeader("Access-Control-Allow-Origin", "*");
-		return "success";
-	}
 	
-	//执行删除操作，并产生删除状态
-	public String DeletePassenger(){
-		HttpServletRequest request = ServletActionContext.getRequest();
-		String pass_id = request.getParameter("pass_id");
-		int isDeletedSuccess=0;
-		try {
-			SubDao passengerDao;
-			passengerDao = new SubDao();
-			passengerDao.openDB();
-			String sql = "delete from t_passenger where pass_id="+pass_id;
-			isDeletedSuccess = passengerDao.executeUpdate(sql);
-			passengerDao.closeDB();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		ServletActionContext.getResponse().setHeader("Access-Control-Allow-Origin", "*");
-		map.put("isDeletedSuccess", isDeletedSuccess);
-		return "success";
-	}
 	
 	private int pass_id;
 	private String pass_name;
@@ -171,6 +104,81 @@ public class PassengerManageAction {
 
 	public void setPass_email(String pass_email) {
 		this.pass_email = pass_email;
+	}
+	
+	
+
+	public Map<String, Object> getMap() {
+		return map;
+	}
+	//产生PassengerManage.jsp页面数据
+	public String GenerateData() throws IOException, SQLException{
+		
+
+		System.out.println(this.getPass_name());
+		
+		
+		ArrayList<Passenger> alist = new ArrayList<Passenger>();
+		int count = 0;
+		SubDao passengerdao,passengerdao2;
+		passengerdao = new SubDao();
+		passengerdao2 = new SubDao();
+		passengerdao.openDB();
+		passengerdao2.openDB();
+		String sql = "select * from t_passenger";
+		ResultSet rs = passengerdao.executeQuery(sql);
+		while(rs.next()){
+			Passenger p=new Passenger();
+			//得到当前乘客的购票数量和购票金额
+			String sql2 = "select count(*) as count,sum(price_purc) as sum from t_order where pass_id="+rs.getInt("pass_id");
+			ResultSet rs2 = passengerdao2.executeQuery(sql2);
+			if(rs2.next()){
+				p.setPass_count(rs2.getInt("count"));
+				p.setPass_amount(rs2.getDouble("sum"));
+			}
+			
+			p.setPass_id(rs.getInt("pass_id"));
+			p.setPass_name(rs.getString("pass_name"));
+			p.setPass_age(rs.getInt("pass_age"));
+			if(rs.getInt("pass_sex")==1){
+				p.setPass_sex("男");
+			}else{
+				p.setPass_sex("女");
+			}
+			p.setPass_idcard(rs.getString("pass_idcard"));
+			p.setPass_passport(rs.getString("pass_passport"));
+			p.setPass_phone(rs.getString("pass_phone"));
+			p.setPass_email(rs.getString("pass_email"));
+			alist.add(p);
+			count++;
+		}
+		passengerdao.closeDB();
+		passengerdao2.closeDB();
+		map.put("status", "success");
+		map.put("totals",count);
+		map.put("data", alist);
+		ServletActionContext.getResponse().setHeader("Access-Control-Allow-Origin", "*");
+		return "success";
+	}
+	
+	//执行删除操作，并产生删除状态
+	public String DeletePassenger(){
+		HttpServletRequest request = ServletActionContext.getRequest();
+		String pass_id = request.getParameter("pass_id");
+		int isDeletedSuccess=0;
+		try {
+			SubDao passengerDao;
+			passengerDao = new SubDao();
+			passengerDao.openDB();
+			String sql = "delete from t_passenger where pass_id="+pass_id;
+			isDeletedSuccess = passengerDao.executeUpdate(sql);
+			passengerDao.closeDB();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		ServletActionContext.getResponse().setHeader("Access-Control-Allow-Origin", "*");
+		map.put("isDeletedSuccess", isDeletedSuccess);
+		return "success";
 	}
 	
 	public String AddPassenger() throws IOException{
